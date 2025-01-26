22e8855254f0fcda1ee5fa08d91d74215ba23a16
Varun Ahlawat <110617721+yup-VARUN@users.noreply.github.com>
2025-01-26 03:41:14 -0500

    removed unnecessary files

--- Package/state_estimator.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class state_estimator:
-    def __init__(self):
-        self.state_vector = {"":""}
-        pass
-    def state_estimation(self, time):
-        self.state_vector = self.state_vector
-        return self.state_vector
